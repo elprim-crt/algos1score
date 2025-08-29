d64b8d052e2bb665802568863195ac7c2b6e5df9
elprim-crt <crt@elprim.si>
2025-08-29 05:00:41 +0200

    Add config.php file for database credentials

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'algos1score');
+define('DB_USER', 'algos1score_user');
+define('DB_PASS', 'algos1score_password');
